bd72a561dafa6c33701913349acccd446fdf8483
Vojtěch Mareš <iam@vojtechmares.com>
2020-11-24 22:53:49 +0100

    Refactoring: uploading files with s3manager instead of s3
    Uploading files using s3manager.Uploader instead of pure s3 service client and manual iteration over files to upload, using directoryIterator instead

--- aws/aws.go
+++ aws/aws.go
@@ -1,7 +1,6 @@
 package aws
 
 import (
-	"fmt"
 	"log"
 	"mime"
 	"os"
@@ -12,6 +11,7 @@ import (
 	amazon_credentials "github.com/aws/aws-sdk-go/aws/credentials"
 	amazon_session "github.com/aws/aws-sdk-go/aws/session"
 	amazon_s3 "github.com/aws/aws-sdk-go/service/s3"
+	amazon_s3manager "github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 type AWS struct {
@@ -23,6 +23,16 @@ type AWS struct {
 type AWSCredentials struct {
 	AccessKeyID string
 	SecretKey   string
+}
+
+type directoryIterator struct {
+	filePaths []string
+	bucket    string
+	next      struct {
+		path string
+		f    *os.File
+	}
+	err error
 }
 
 func NewAWS(l *log.Logger, c *AWSCredentials, region string) *AWS {
@@ -48,47 +58,77 @@ func NewAWS(l *log.Logger, c *AWSCredentials, region string) *AWS {
 }
 
 func (aws *AWS) UploadFilesFromDir(b *Bucket, dir string) {
-	var files []string
+	svc := aws.getS3()
+	iter := newDirectoryIterator(b.Name, dir)
+	uploader := amazon_s3manager.NewUploaderWithClient(svc)
 
 	aws.logger.Println("Uploading files...")
 
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
-
-		files = append(files, path)
-		return nil
-	})
-	if err != nil {
-		aws.logger.Fatalf("Error while reading upload dir\n%v", err)
+	if err := uploader.UploadWithIterator(amazon_aws.BackgroundContext(), iter); err != nil {
+		aws.logger.Fatalf("Error while uploading files to bucket\n%v", err)
 	}
 
-	svc := aws.getS3()
-
-	for _, file := range files {
-		f, err := os.Open(file)
-		if err != nil {
-			aws.logger.Fatalf("Could not open file\n%v", err)
-		}
-		defer f.Close()
-
-		key := strings.Replace(file, fmt.Sprintf("%s/", dir), "", 1)
-
-		aws.logger.Printf("Uploading %s to %s\n", file, key)
-
-		_, err = svc.PutObject(&amazon_s3.PutObjectInput{
-			Bucket:      amazon_aws.String(b.Name),
-			Key:         amazon_aws.String(key),
-			Body:        f,
-			ContentType: amazon_aws.String(mime.TypeByExtension(filepath.Ext(file))),
-		})
-		if err != nil {
-			aws.logger.Fatalf("Error while uploading file\n%v", err)
-		}
-	}
+	aws.logger.Println("Upload complete")
 }
 
 func (aws *AWS) getS3() *amazon_s3.S3 {
 	return amazon_s3.New(aws.session)
+}
+
+func newDirectoryIterator(bucket, dir string) amazon_s3manager.BatchUploadIterator {
+	paths := []string{}
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		// We care only about files, not directories
+		if !info.IsDir() {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+
+	return &directoryIterator{
+		filePaths: paths,
+		bucket:    bucket,
+	}
+}
+
+func (iter *directoryIterator) UploadObject() amazon_s3manager.BatchUploadObject {
+	f := iter.next.f
+	key := iter.next.path
+	contentType := mime.TypeByExtension(filepath.Ext(iter.next.path))
+
+	if i := strings.Index(iter.next.path, "/"); i != -1 {
+		key = iter.next.path[i:]
+	}
+	return amazon_s3manager.BatchUploadObject{
+		Object: &amazon_s3manager.UploadInput{
+			Bucket:      &iter.bucket,
+			Key:         &key,
+			Body:        f,
+			ContentType: &contentType,
+		},
+
+		After: func() error {
+			return f.Close()
+		},
+	}
+}
+
+func (iter *directoryIterator) Next() bool {
+	if len(iter.filePaths) == 0 {
+		iter.next.f = nil
+		return false
+	}
+
+	f, err := os.Open(iter.filePaths[0])
+	iter.err = err
+
+	iter.next.f = f
+	iter.next.path = iter.filePaths[0]
+
+	iter.filePaths = iter.filePaths[1:]
+	return true && iter.Err() == nil
+}
+
+func (iter *directoryIterator) Err() error {
+	return iter.err
 }
